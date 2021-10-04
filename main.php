b158b886b66bb8856cce89453d8da6d09455edc8
Pedro Figueroa <figueroash@gmail.com>
2021-10-04 16:01:47 -0300

    Creating the plugin

---
+++ main.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Learning Tapestry Coding Challenge
+ * Plugin URI: https://github.com/mardelnet/lt-coding-challenge
+ * Description: Coding Challenge: create a custom WordPress plugin to manage and render a list of courses as shown on the provided designs.
+ * Version: 1.0
+ * Author: Pedro Figueroa
+ * Author URI: https://www.linkedin.com/in/pedrofigueroa1989/
+ */
